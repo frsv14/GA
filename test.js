a2093a796165e8eb7dffb40ea75ad1b46cbf71e5
frsv14 <naf.freddy04@gmail.com>
2022-10-17 10:06:11 +0200

    Room creation
    börjar att använda flems för att fortsätta med utveklingen medans jag fortsätter att leta efter ett fix till "e" problemet

---
+++ test.js
@@ -0,0 +1,15 @@
+let string = "something";
+
+function stringconv (string){
+
+    let stringlength = length.string;
+
+    for (stringlength ; stringlength > -1 ; --stringlength){
+         
+        console.log(stringlength);
+
+    }
+
+
+
+}
